57f169c911bba641b5b304e708a4b656a9d476e0
AnujJandhyala12 <anujjandhyala12@gmail.com>
2026-04-25 18:07:02 +0530

    Modified server.js and package.json

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
+const admin = require("firebase-admin");
+console.log("Firebase Admin Loaded");
+
 const app = express();
 
 app.use(cors());
