6741460e021669789ea30946a533559c2a737251
AnujJandhyala12 <anujjandhyala12@gmail.com>
2026-05-03 20:26:07 +0530

    add event_type to matches

--- server.js
+++ server.js
@@ -157,13 +157,15 @@ app.get("/matches/live", (req, res) => {
         team1: "MU-A",
         team2: "BITS Hyderabad",
         score: "MU - 179/3 (15.4 overs)",
-        status: "Live"
+        status: "Live",
+        event_type: "AIRO"      // 🔥 ADD THIS
       },
       {
         team1: "Gitam",
         team2: "MU-B",
         score: "Gitam 123/5 (12.5 overs)",
-        status: "Live"
+        status: "Live",
+        event_type: "AETHER"    // 🔥 ADD THIS
       }
     ]
   });
